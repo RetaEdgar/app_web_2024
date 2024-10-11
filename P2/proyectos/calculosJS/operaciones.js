9284f900f2d55abd9f403c0005dee4343764ae82
RetaEdgar <edgarreta39@gmail.com>
2024-10-11 09:54:43 -0600

    1ER COMMIT REPASO_HTML_CSS_JS

--- P2/proyectos/calculosJS/operaciones.js
+++ P2/proyectos/calculosJS/operaciones.js
@@ -1,22 +1,33 @@
 function operacion()
 {
-    let n1=parseInt(document.getElementById("n1").value);
-    let n2=parseInt(document.getElementById("n2").value);
-    let tipoope=document.getElementById("tipo").value;
+    let n1, n2, tipoope,resultado,ope;
+    n1=parseFloat(document.getElementById("n1").value);
+    n2=parseFloat(document.getElementById("n2").value);
+    tipoope=document.getElementById("tipo").value;
 
-    // <--switch se utiliza como el segun -->
-    switch (tipoope)
+    if (isNumber (n1) && isNumber(n2) )
     {
-        case "suma":ope=n1+n2;break;
-        case "resta": ope = n1-n2;break;
-        case "multiplicacion": ope=n1*n2; break;
-        case "division":ope=n1/n2;break;
-        
-    }
+        // <--switch se utiliza como el segun -->
+        switch (tipoope) {
+            case "+": ope = n1 + n2; break;
+            case "-": ope = n1 - n2; break;
+            case "*": ope = n1 * n2; break;
+            case "/": ope = n1 / n2; break;
 
-    let resultado=document.getElementById("resultado");
-    resultado.innerHTML=`<h2> El resultado de la ${tipoope} de ${n1} + ${n2} el resultado es = ${ope}</h2>`
-// CALCULADORA
+        }
+
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = `<h2> ${n1} ${tipoope} ${n2} = ${ope}</h2>`
+    }
+    else
+        // resultado=document.getElementById("resultado").innerHTML=`<h2>Ingresa solo numeros</h2>`
+        alert("Ingresa solo numeros")
+
     
+}
+
+function isNumber(n)
+{
+    return !isNaN(parseInt(n) && isFinite(n))
 }
 
